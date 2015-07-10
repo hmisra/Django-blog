dd09f1b65c15b93de5f76070fd04b6650fdd4a7e
unknown <hmisra@usc.edu>
2015-07-10 01:15:56 -0700

    recursivebayes

--- helloworld/admin.py
+++ helloworld/admin.py
@@ -5,7 +5,7 @@ from helloworld.models import Blogs
 
 class BlogsAdmin(admin.ModelAdmin):
     def formfield_for_dbfield(self, db_field, **kwargs):
-        formfield = super(CabAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+        formfield = super(BlogsAdmin, self).formfield_for_dbfield(db_field, **kwargs)
         if db_field.name == 'content':
             formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
         return formfield
